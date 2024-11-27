d892d61f33cb8858850f8e93d94fb2e44c218baa
yuseong choi <yschoi9733@gmail.com>
2024-11-27 13:15:27 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { DragDropContext, Draggable, Droppable } from '@hello-pangea/dnd';
+
+const finalSpaceCharacters = [
+  {
+    id: 'gary',
+    name: 'Gary Goodspeed',
+  },
+  {
+    id: 'cato',
+    name: 'Little Cato',
+  },
+  {
+    id: 'kvn',
+    name: 'KVN',
+  },
+]
 
 function App() {
+  const [characters, setCharacters] = useState(finalSpaceCharacters);
+
+  const handleEnd = (result) => {
+    // result 매개변수에는 source 항목 및 대상 위치와 같은 드래그 이벤트에 대한 정보가 포함.
+    
+    // 목적지가 없으면 이 함수를 종료
+    if (!result.destination) return;
+
+    // 리액트 불변성을 지켜주기 위해 새로운 todoData 생성
+    const items = Array.from(characters);
+
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    setCharacters(items);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Final Space Characters</h1>
+        <DragDropContext onDragEnd={handleEnd}>
+          <Droppable droppableId='characters'>
+            {(provided) => (
+              <ul 
+                className='characters'
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                {
+                  characters.map(({id, name}, index) => (
+                    <Draggable 
+                      key={id} 
+                      draggableId={id} 
+                      index={index}
+                    >
+                      {(provided) => (
+                        <li 
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          <p>
+                            {name}
+                          </p>
+                        </li>
+                      )}
+                    </Draggable>
+                  ))
+                }
+                {provided.placeholder}
+              </ul>
+            )}
+          </Droppable>
+        </DragDropContext>
       </header>
     </div>
   );
